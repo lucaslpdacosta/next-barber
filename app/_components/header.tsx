92c20e5c4ff3fe0418e521dbbb9d496d7bf63e74
Lucas Prudencio <lucaslpdacosta1@gmail.com>
2025-04-26 11:49:50 -0300

    chore: updated shadcn components configs

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -6,7 +6,7 @@ import { MenuIcon } from "lucide-react"
 const Header = () => {
   return (
     <Card>
-      <CardContent className="flex flex-row items-center justify-between">
+      <CardContent className="flex flex-row items-center justify-between p-5">
         <Image alt="FSW Barber" src="/logo.png" height={18} width={120} />
         <Button size="icon" variant="outline">
           <MenuIcon />
