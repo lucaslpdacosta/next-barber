b62bfd71eeca705b400b9d02b3d2b6b827866bd3
Lucas Prudencio <lucaslpdacosta1@gmail.com>
2025-05-09 18:04:28 -0300

    feat: implemented searching mechanism and interface tweakings

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
 import Header from "./_components/header"
-import { Input } from "./_components/ui/input"
 import { Button } from "./_components/ui/button"
-import { SearchIcon } from "lucide-react"
 import Image from "next/image"
 import { db } from "./_lib/prisma"
 import BarbershopItem from "./_components/barbershop-item"
 import { quickSearchOptions } from "./_constants/search"
 import BookingItem from "./_components/booking-item"
+import Search from "./_components/search"
 
 const Home = async () => {
   const barbershops = await db.barbershop.findMany({})
@@ -17,11 +16,9 @@ const Home = async () => {
       <div className="p-5">
         <h2 className="text-xl font-bold">Olá, Usuário</h2>
         <p>Dia</p>
-        <div className="mt-6 flex items-center gap-2">
-          <Input placeholder="Faça sua busca..." />
-          <Button>
-            <SearchIcon />
-          </Button>
+
+        <div className="mt-6">
+          <Search />
         </div>
 
         <div className="mt-6 flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
